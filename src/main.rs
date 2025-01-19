9cfec633d0a3d02ab13c153bc4b8b325eee9f4b0
Telmo Carmo <telmo.carmo@gmail.com>
2025-01-19 18:36:43 +0000

    1st commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+/*
+
+rustup target add wasm32-unknown-unknown
+cargo install trunk wasm-bindgen-cli
+
+trunk serve
+
+--
+
+wasm-pack build --target web
+
+python3 -m http.server
+
+
+ */
+
+ use yew::prelude::*;
+ //use web_sys::HtmlInputElement;
+
+ #[derive(Properties, PartialEq)]
+ pub struct Props {
+     pub name: String,
+ }
+ 
+ #[function_component(Greeting)]
+ fn greeting(props: &Props) -> Html {
+     html! {
+         <p>{ format!("Hello, {}!", props.name) }</p>
+     }
+ }
+ 
+ #[function_component(App)]
+ fn app() -> Html {
+     let counter = use_state(|| 0);
+     let name = use_state(|| String::from("World")); // State for the name
+     let name_input_ref = NodeRef::default(); // Ref for the input element
+ 
+     let update_name = {
+         let name = name.clone();
+         let name_input_ref = name_input_ref.clone(); // Clone for use in the closure
+         Callback::from(move |_| {
+             if let Some(input) = name_input_ref.cast::<web_sys::HtmlInputElement>() {
+                 name.set(input.value());
+             }
+         })
+     };
+
+     let on_bt1 = {
+        let counter = counter.clone();
+        Callback::from(move |_| {
+            web_sys::console::log_1(&"Button clicked!".into());
+            let value = *counter + 1;
+            counter.set(value);
+        })
+     };
+ 
+     html! {
+         <div>
+             <h1>{ "Yew App 1 Main" }</h1>
+             <input type="text" ref={name_input_ref} placeholder="Enter your name" onchange={update_name}/>
+             <Greeting name={(*name).clone()} />
+ 
+             <h2>{"List Example"}</h2>
+             <ul>
+                 {
+                     (1..=5).map(|i| {
+                         html! { <li>{ format!("Item {}", i) }</li> }
+                     }).collect::<Html>()
+                 }
+             </ul>
+ 
+             <h2>{"Conditional Rendering Example"}</h2>
+             {
+                 if *name == "Yew" {
+                     html! { <p style="color: green;">{"Welcome, Yew!"}</p> }
+                 } else {
+                     html! { <p style="color: blue;">{"You are not Yew."}</p> }
+                 }
+             }
+ 
+             <h2>{"Handling Events"}</h2>
+             <p>{ *counter }</p>
+             <button onclick={on_bt1}>{"Click Me"}</button>
+         </div>
+     }
+ }
+ 
+ fn main() {
+    yew::Renderer::<App>::new().render();
+ }
